f5f28cedb7b5586969973a4339fe34b664325548
anoopezhap <anoopezhap@gmail.com>
2024-02-27 20:08:07 -0500

    Server connected

--- api/index.js
+++ api/index.js
@@ -1,6 +1,31 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const { dot } = require("node:test/reporters");
+
+dotenv.config();
 
 const app = express();
+
+const uri = process.env.MONGODB_URI;
+
+const clientOptions = {
+  serverApi: { version: "1", strict: true, deprecationErrors: true },
+};
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
