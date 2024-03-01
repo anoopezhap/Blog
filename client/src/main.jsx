3f4aca7adf7b9dddbb083095655452242a0a4d8d
anoopezhap <anoopezhap@gmail.com>
2024-02-29 22:47:16 -0500

    Redux persist done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,18 +4,21 @@ import App from "./App.jsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import store from "./store.js";
+import { store, persistor } from "./store.js";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={false} />
-        <App />
-      </QueryClientProvider>
-    </Provider>
+    <PersistGate persistor={persistor}>
+      <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
+          <ReactQueryDevtools initialIsOpen={false} />
+          <App />
+        </QueryClientProvider>
+      </Provider>
+    </PersistGate>
   </React.StrictMode>
 );
